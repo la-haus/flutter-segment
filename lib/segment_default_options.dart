248c01bf661796143994d09f153a4eba872dd7dd
Maik Schmidt <maik.schmidt.hl@gmail.com>
2020-02-13 15:42:59 +0100

    feat: use default options in new implementation

---
+++ lib/segment_default_options.dart
@@ -0,0 +1,8 @@
+class SegmentDefaultOptions {
+  Map<String, dynamic> options;
+
+  /// Singleton of [SegmentDefaultOptions].
+  static final SegmentDefaultOptions instance = SegmentDefaultOptions._();
+
+  SegmentDefaultOptions._();
+}
