a3c56efad0b5649813aa042322ecd6a8b14b51e0
Maik Schmidt <maik.schmidt.hl@gmail.com>
2020-02-14 15:23:05 +0100

    refactor: entrypoint must fit the library name

--- lib/src/segment_observer.dart
+++ lib/src/segment_observer.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_segment/segment.dart';
+import 'package:flutter_segment/flutter_segment.dart';
 
 typedef String ScreenNameExtractor(RouteSettings settings);
 
