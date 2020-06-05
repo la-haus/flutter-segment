84642e8b1a6aec7f1c578bab0c910ed1e9ad81a8
Maik Schmidt <maik.schmidt.hl@gmail.com>
2020-06-05 14:25:15 +0200

    refactor: rename variables

--- ios/Classes/FlutterSegmentPlugin.m
+++ ios/Classes/FlutterSegmentPlugin.m
@@ -14,7 +14,7 @@ static NSDictionary *_appendToContextMiddleware;
     NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
     NSString *writeKey = [dict objectForKey: @"com.claimsforce.segment.WRITE_KEY"];
     BOOL trackApplicationLifecycleEvents = [[dict objectForKey: @"com.claimsforce.segment.TRACK_APPLICATION_LIFECYCLE_EVENTS"] boolValue];
-    BOOL enableBranchMetrics = [[dict objectForKey: @"com.claimsforce.segment.ENABLE_BRANCH_IO_INTEGRATION"] boolValue];
+    BOOL isBranchIoIntegrationEnabled = [[dict objectForKey: @"com.claimsforce.segment.ENABLE_BRANCH_IO_INTEGRATION"] boolValue];
     SEGAnalyticsConfiguration *configuration = [SEGAnalyticsConfiguration configurationWithWriteKey:writeKey];
 
     // This middleware is responsible for manipulating only the context part of the request,
@@ -102,11 +102,11 @@ static NSDictionary *_appendToContextMiddleware;
     ];
 
     configuration.trackApplicationLifecycleEvents = trackApplicationLifecycleEvents;
-    
-    if(enableBranchMetrics){
+
+    if (isBranchIoIntegrationEnabled) {
       [configuration use:[BNCBranchIntegrationFactory instance]];
     }
-   
+
     [SEGAnalytics setupWithConfiguration:configuration];
     FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"flutter_segment"
