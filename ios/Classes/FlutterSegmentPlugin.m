8fb6f0860c4c2188470456d33cddf6adfaebfbf0
Daniel Gomez Rico <danielgomezrico@gmail.com>
2021-12-17 17:26:09 -0500

    Remove unwanted logs

--- ios/Classes/FlutterSegmentPlugin.m
+++ ios/Classes/FlutterSegmentPlugin.m
@@ -118,8 +118,6 @@ static BOOL wasSetupFromFile = NO;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  NSLog(@"SegmentFlutter - iOS - Method %@, args: %@", call.method, call.arguments);
-
   if ([@"config" isEqualToString:call.method] && !wasSetupFromFile) {
     [self config:call result:result];
   } else if ([@"identify" isEqualToString:call.method]) {
@@ -182,19 +180,13 @@ static BOOL wasSetupFromFile = NO;
 
 - (void)identify:(FlutterMethodCall*)call result:(FlutterResult)result {
   @try {
-    NSLog(@"SegmentFlutter - iOS - 1");
     NSString *userId = call.arguments[@"userId"];
-    NSLog(@"SegmentFlutter - iOS - 2 %@", userId);
     NSDictionary *traits = call.arguments[@"traits"];
-    NSLog(@"SegmentFlutter - iOS - 3 %@", traits);
     NSDictionary *options = call.arguments[@"options"];
-    NSLog(@"SegmentFlutter - iOS - 4 %@", options);
 
     [[SEGAnalytics sharedAnalytics] identify: userId
                       traits: traits
                      options: options];
-
-    NSLog(@"SegmentFlutter - iOS - 5 %@", options);
 
     result([NSNumber numberWithBool:YES]);
   }
