4ae5b3b3a2e167c7b510469c952e32ecb4d209bb
Matthieu Pernelle <matthieu.pernelle@gmail.com>
2022-03-31 18:17:18 +0200

    :rotating_light: (remove warning implementation_imports by exporting segment_web)
    see https://dart-lang.github.io/linter/lints/implementation_imports.html

--- example/lib/generated_plugin_registrant.dart
+++ example/lib/generated_plugin_registrant.dart
@@ -5,8 +5,7 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
-// ignore: implementation_imports
-import 'package:flutter_segment/src/segment_web.dart';
+import 'package:flutter_segment/segment_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
